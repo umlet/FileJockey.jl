dcd1f0e9f6228a4e14459ef17da7f18edf4deb81
Martin <martin@umlet.com>
2023-03-26 15:56:34 +0200

    hardlinker

--- src/exify.jl
+++ src/exify.jl
@@ -38,14 +38,15 @@ struct ExifData
     function ExifData(d::OrderedDict{String, Any})
         _d = OrderedDict{Symbol, Any}()
 
-        # original/full keys as symbols
+        # original (":"=>"__"-corrected) keys as symbols
         for (k,v) in d  _d[Symbol(k)] = v  end
 
         # shortcut keys, if unique
         dtmp = OrderedDict()
         for (korig,_) in d
-            ss = split(korig, '_')
-            length(ss) != 2  &&  continue  # must be 2; >2 not possible; 1 possible for 'SourceFile'
+            ss = split(korig, "__")
+            @assert length(ss) == 1  ||  length(ss) == 2
+            length(ss) == 1  &&  continue  # possible for 'SourceFile'
             kshort = ss[2]
             haskey(dtmp, kshort)  ||  ( dtmp[kshort] = [] )
             push!(dtmp[kshort], korig)
@@ -69,9 +70,9 @@ Base.propertynames(x::ExifData) = Tuple(keys(x._d))
 function changeexifkeys(d::OrderedDict{String, Any})
     RET = OrderedDict{String, Any}()
     for (k,v) in d
-        occursin('_', k)  &&  error("exif dict key '$(k)' contains '_'")
+        occursin("__", k)  &&  error("exif dict key '$(k)' contains '__'")
         count(':', k) >= 2  &&  error("exif dict key '$(k)' contains 2+ colons")
-        k2 = replace(k, ':'=>'_')
+        k2 = replace(k, ':'=>"__")
         RET[k2] = v
     end
     return RET
@@ -79,15 +80,27 @@ end
 
 
 
-function info(x::ExifData)
+function info(x::ExifData, grep=nothing)
     len = keys(x._d) |> mp(string) |> mp(length) |> maximum
     for (k,v) in x._d
-        println(rpad(k, len), " = ", v)
+        if grep !== nothing
+            !occursin(grep, string(k))  &&  continue
+        end
+        println(":", rpad(k, len), " = ", v)
     end
 end
 
-function _exif_date2sanedate(s::AbstractString)
-    length(s) != 19  &&  error("exif value of 'Time_CreateDate' has length != 19")
+
+
+
+
+
+
+
+
+function _date2sanedatetime(s::AbstractString)
+    # time with and without time zone correction
+    length(s) in (19, 25)  ||  error("invalid date format in '$(s)'")
     YYYY = s[1:4]
     MM = s[6:7]
     DD = s[9:10]
@@ -97,19 +110,93 @@ function _exif_date2sanedate(s::AbstractString)
     return YYYY * MM * DD * "_" * hh * mm * ss
 end
 
-function exif_create_datetime(x::ExifData)
-    if haskey(x._d, :Time_CreateDate)
-        s = x.Time_CreateDate
-    end
-    return _exif_date2sanedate(s)
+function exif_sane_datetime(x::ExifData)
+    haskey(x._d, :Time__CreateDate)  &&  ( return _date2sanedatetime(x.Time__CreateDate) )
+    haskey(x._d, :Time__FileModifyDate)  &&  ( return _date2sanedatetime(x.Time__FileModifyDate) )
+    return "unknown"
+end
+function exif_sane_date(x::ExifData)
+    s = exif_sane_datetime(x)
+    s == "unknown"  &&  ( return s )
+    s = split(s, "_")[1]
+    @assert length(s) == 8
+    return s
+end
+function exif_sane_year(x::ExifData)
+    s = exif_sane_date(x)
+    s == "unknown"  &&  ( return s )
+    return s[1:4]
+end
+function exif_sane_yearmonth(x::ExifData)
+    s = exif_sane_date(x)
+    s == "unknown"  &&  ( return s )
+    return s[1:6]
 end
 
 
 
 
 
+function _dnames_and_hardlink(f::FileEntry, x::ExifData, dname)
+    RET_dnames_full = String[]
 
+    fname_base = basename(f)
+    lname_base = exif_sane_datetime(x) * "___" * fname_base
 
+    dname_rel_1 = exif_sane_year(x)
+    dname_full_1 = joinpath(dname, dname_rel_1)
+    push!(RET_dnames_full, dname_full_1)
+    if dname_rel_1 == "unknown"
+        RET_lname_full = joinpath(dname_full_1, lname_base)
+        return (RET_dnames_full, RET_lname_full)
+    end
+
+    dname_rel_2 = exif_sane_yearmonth(x)
+    dname_full_2 = joinpath(dname_full_1, dname_rel_2)
+    push!(RET_dnames_full, dname_full_2)
+    RET_lname_full = joinpath(dname_full_2, lname_base)
+    return (RET_dnames_full, RET_lname_full)
+end
+
+function hardlinker(F::AbstractVector{<:FileEntry}, X::AbstractVector{ExifData}, dname)
+    dname = path(DirEntry(dname))
+    length(F) != length(X)  &&  error("length mismatch files and exifdata")
+
+    dnames_full = String[]
+    lnames_full = String[]
+    for (f,x) in zip(F, X)
+        _dnames_full,_lname_full = _dnames_and_hardlink(f, x, dname)
+        append!(dnames_full, _dnames_full)
+        push!(lnames_full, _lname_full)
+    end
+
+    #check for unique target filenames
+    if length(Set(lnames_full)) != length(lnames_full)
+        error("link paths are not unique!")
+    else
+        @info "$(length(lnames_full)) unique links will be created"
+    end
+    # make sure none of the target hardlinks exist
+    for lname_full in lnames_full
+        ispath(lname_full)  &&  error("path '$(lname_full)' already exists")
+    end
+    @info "(..none of them exists)"
+
+    # create intermediate dirs
+    dnames_full = OrderedSet(dnames_full) |> collect
+    @info "hardlinks will reside in $(length(dnames_full)) dirs; creating intermediate dirs.."
+
+    for dname_full in dnames_full
+        isdir(dname_full)  &&  continue
+        mkdir(dname_full)
+    end
+    @info "dirs created"
+    
+    for (f,lname_full) in zip(F, lnames_full)
+        println(path(f), " <-- ", lname_full, " created")
+        hardlink(path(f), lname_full)
+    end
+end
 
 
 
