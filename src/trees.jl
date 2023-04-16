3bd59acfc43b318b6e3c84fef2af523e9e696e7e
Martin <martin@umlet.com>
2023-04-16 11:18:04 +0200

    docs

--- src/trees.jl
+++ src/trees.jl
@@ -15,6 +15,19 @@ end
 fsreaddir(s::AbstractString=".") = fsreaddir(Entry(s))
 
 
+ls(x::DirEntry) = fsreaddir(x)
+ls(x::Symlink{DirEntry}) = fsreaddir(x.target)
+ls(x::AbstractEntry) = [x]
+
+ls(s::AbstractString) = ls(Entry(s))
+ls() = ls(".")
+
+ls(X::AbstractVector) = ls.(X) |> flatten
+
+ll(args...) = ls(args...)
+
+
+
 
 struct FsTreeIter
     state::Vector{AbstractEntry}
@@ -30,17 +43,6 @@ _treechildren(x::Union{DirEntry, Symlink{DirEntry}}) = fsreaddir(x)
 _treechildren(x::AbstractEntry) = AbstractEntry[]
 
 Base.iterate(x::FsTreeIter) = iterate(x, nothing)
-### ORIG
-# function Base.iterate(x::FsTreeIter, ::Nothing)
-#     length(x.state) == 0  &&  return nothing
-
-#     item = popfirst!(x.state)
-
-#     children = _treechildren(item)
-#     length(children) > 0  &&  ( prepend!(x.state, children) )
-
-#     return (item, nothing)
-# end
 function Base.iterate(x::FsTreeIter, ::Nothing)
 @label retry
     length(x.state) == 0  &&  return nothing
@@ -57,19 +59,26 @@ end
 fswalkdir(x::AbstractEntry; skip_paths=String[]) = FsTreeIter(x, skip_paths)  # TODO skip_paths everywhere
 fswalkdir(s::AbstractString="."; skip_paths=String[]) = fswalkdir(Entry(s); skip_paths=skip_paths)
 
+
+
+
 eachentry(args...; skip_paths=String[]) = fswalkdir(args...; skip_paths=skip_paths)
-#eachfile(args...) = eachentry(args...) |> fl(isfile) |> mp(follow)
+eachentry(X::AbstractVector; skip_paths=String[]) = eachentry.(X; skip_paths=skip_paths) |> flatten_ 
 
-ls(x::DirEntry) = fsreaddir(x)
-ls(x::Symlink{DirEntry}) = fsreaddir(x.target)
-ls(s::AbstractString=".") = ls(Entry(s))
-ls(x::AbstractEntry) = [x]
+eachfile(args...) = eachentry(args...) |> fl_(isfile) |> mp_(follow)
 
-ll(args...) = ls(args...)
 
-find(x::AbstractEntry; skip_paths=String[]) = fswalkdir(x; skip_paths=skip_paths) |> cl
-find(s::AbstractString="."; skip_paths=String[]) = find(Entry(s); skip_paths=skip_paths)
 
-find(X::AbstractVector; skip_paths=String[]) = find.(X; skip_paths=skip_paths) |> flatten
+
+
+find(args...; skip_paths=String[]) = eachentry(args...; skip_paths=skip_paths) |> cl
+#find(x::AbstractEntry; skip_paths=String[]) = fswalkdir(x; skip_paths=skip_paths) |> cl
+#find(s::AbstractString="."; skip_paths=String[]) = find(Entry(s); skip_paths=skip_paths)
+#find(X::AbstractVector; skip_paths=String[]) = find.(X; skip_paths=skip_paths) |> flatten
+
+findfiles(args...) = find(args...) |> 
+
+
 
 include("trees.jl_exports")
+include("trees.jl_docs")
